5354eb72a6bcafd9d7cf72e68ba2e0a86bb10516
shubhamkarn <shubhamkarn023@gmail.com>
2025-03-12 12:57:20 +0530

    Lenskart Clone

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) currentSlide = 0;
+    if (index < 0) currentSlide = slides.length - 1;
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-slides').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    currentSlide++;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide--;
+    showSlide(currentSlide);
+}
+
+// Auto-play carousel (optional)
+setInterval(nextSlide, 5000);
